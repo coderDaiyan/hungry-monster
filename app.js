17d35e9ac3dd9b8ec976f906c48ff6663208bf4b
coderDaiyan <daiyanabdallah@gmail.com>
2021-02-07 14:54:43 +0600

    some things are done

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 const searchBtn = document.getElementById("meal-btn")
-searchBtn.addEventListener('click', function(){
+searchBtn.addEventListener('click', () => {
     let searchInput = document.getElementById('meal-input').value
     const mealList = document.getElementById('meal-list')
     fetch(`https://www.themealdb.com/api/json/v1/1/filter.php?i=${searchInput}`)
@@ -9,17 +9,23 @@ searchBtn.addEventListener('click', function(){
         if(data.meals){
             data.meals.forEach(meal => {
                 result += `
-                <div class="meal-item" data-id=${meal.idMeal}>
-                    <div class="meal-thumb">
+                <div id="meal-item" class="meal-item" data-id=${meal.idMeal}>
+                    <div>
                         <img src=${meal.strMealThumb} alt="meal" />
                     </div>
-                    <div class="meal-name">
-                        <h2>${meal.strMeal}</h2>
+                    <div>
+                        <h2 id="meal-name">${meal.strMeal}</h2>
                     </div>
               </div>
                 `
             });
+        } else {
+            result = `
+            <h1 id="sorry-message">Sorry. We have no recipes in this name ${searchInput}</h1>
+            `
         }
         mealList.innerHTML = result;
     })
 })
+
+
